9f8a25ed50eb12d646606dfa61e7897f7519aa61
William Robert <willtaavares@gmail.com>
2021-10-19 11:30:33 -0300

    feat: add redux and first configs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import { Provider } from 'react-redux';
 import { Routes } from './routes';
 import GlobalStyle from './styles/global';
 
+import store from './store';
+
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <GlobalStyle />
       <Routes />
-    </>
+    </Provider>
   );
 }
 
